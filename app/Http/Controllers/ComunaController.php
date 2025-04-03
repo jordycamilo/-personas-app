2ec05f889baf3845132e099ca76edd0ebb083678
jordy diaz <jordycamilodiazladino@gmail.com>
2025-04-02 22:04:28 -0500

    git

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use     App\Models\Comuna;
+use App\Models\Comuna;
 use Illuminate\Http\Request;
 use illuminate\Support\facades\DB;
 
@@ -13,7 +13,11 @@ class ComunaController extends Controller
      */
     public function index()
     {
-        $comunas = Comuna::all();
+       // $comunas = Comuna::all();
+       $comunas = DB:: table ('tb_comuna')
+       ->join ('tb_municipio','tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+       ->select('tb_comuna.*',"tb_municipio.muni_nomb")
+       ->get();
         return view ('comuna.index',['comunas'=> $comunas]);
     }
 
