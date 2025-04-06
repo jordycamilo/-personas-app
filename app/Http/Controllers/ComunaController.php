1801a8595af8ea5b30e33f513abbf28482bfc4e4
jordy diaz <jordycamilodiazladino@gmail.com>
2025-04-06 15:34:04 -0500

    Commune Edit

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -64,7 +64,13 @@ $comunas = DB::table('tb_comuna')
      */
     public function edit(string $id)
     {
-        //
+        $comuna = Comuna::find($id);
+$municipios = DB::table('tb_municipio')
+    ->orderBy('muni_nomb')
+    ->get();
+
+return view('comuna.edit', ['comuna' => $comuna, 'municipios' => $municipios]);
+
     }
 
     /**
@@ -72,7 +78,19 @@ $comunas = DB::table('tb_comuna')
      */
     public function update(Request $request, string $id)
     {
-        //
+        $comuna = Comuna::find($id);
+
+$comuna->comu_nomb = $request->name;
+$comuna->muni_codi = $request->code;
+$comuna->save();
+
+$comunas = DB::table('tb_comuna')
+    ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+    ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+    ->get();
+
+return view('comuna.index', ['comunas' => $comunas]);
+
     }
 
     /**
